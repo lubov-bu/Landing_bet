0c40e657821f60840bb28bdc62ea85f650ca3665
Lu <62780599+Lubov-L@users.noreply.github.com>
2023-05-13 23:17:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const textMessage = document.querySelector('.popup__h1');
 const textPhar = document.querySelector('.popup__ph');
 
 function showPopup () {
-    console.log("hello");
     popup.classList.toggle("invisible");
 }
 
